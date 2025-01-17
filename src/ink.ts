30dd9ce170666f71012d8d72c5d9ce905e3744ec
Ilya Kowalewski <badt@appar.at>
2025-01-17 19:55:29 +0200

    busthorne: export internals and publish to npm
    Closes #3

--- src/ink.ts
+++ src/ink.ts
@@ -1,4 +1,25 @@
+// Core exports
 export { Story, InkList } from "./engine/Story";
 export { Compiler, CompilerOptions } from "./compiler/Compiler";
 export { PosixFileHandler } from "./compiler/FileHandler/PosixFileHandler";
 export { JsonFileHandler } from "./compiler/FileHandler/JsonFileHandler";
+
+// Engine internals
+export * from "./engine/CallStack";
+export * from "./engine/Choice";
+export * from "./engine/Container";
+export * from "./engine/ControlCommand";
+export * from "./engine/Value";
+export * from "./engine/Object";
+
+// Compiler internals
+export * from "./compiler/Parser/ParsedHierarchy/Story";
+export * from "./compiler/Parser/ParsedHierarchy/Object";
+export * from "./compiler/Parser/ParsedHierarchy/Expression/Expression";
+export * from "./compiler/Parser/ParsedHierarchy/Variable/VariableAssignment";
+export * from "./compiler/Parser/ParsedHierarchy/Flow/FlowBase";
+export * from "./compiler/Parser/ParsedHierarchy/AstSerializer";
+
+// Parser components
+export * from "./compiler/Parser/InkParser";
+export * from "./compiler/Parser/StringParser/StringParser";
